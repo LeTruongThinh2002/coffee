31dc07a6317b2ff7521bc107b67d512cf801c9ab
Lê Trường Thịnh <109189038+LeTruongThinh2002@users.noreply.github.com>
2023-12-23 13:14:49 +0700

    Delete WebClient directory

--- WebClient/resources/views/email/verify.blade.php
+++
@@ -1,8 +0,0 @@
-
-<div>
-    <div>
-        <h1>{{$data['title']}}</h1>
-    </div>
-    <div><button><a href="http://127.0.0.1:9000/auth/verify?token={{$data['token']}}">Click here to active email!</a></button></div>
-</div>
-
